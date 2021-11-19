cf8a37eef9fa8694473a0ede125dd9f878c3b9b8
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-11-19 19:09:22 -0300

    Create Migration Utility project and add Dockerfiles

---
+++ Photobook.MigrationUtility/MigrationUtilityStartup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Photobook.MigrationUtility
+{
+    public class MigrationUtilityStartup
+    {
+        public MigrationUtilityStartup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DatabaseContext>(options =>
+            {
+                options.UseMySql(Configuration.GetConnectionString("LearningAnalyticsAPIContext"));
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
---
+++ Photobook.MigrationUtility/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Photobook.Data;
+using System;
+using System.IO;
+
+namespace Photobook.MigrationUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Applying migrations");
+            var webHost = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<MigrationUtilityStartup>()
+                .Build();
+            using (var context = (PhotobookDbContext)webHost.Services.GetService(typeof(PhotobookDbContext)))
+            {
+                context.Database.Migrate();
+            }
+            Console.WriteLine("Done");
+        }
+    }
+}
