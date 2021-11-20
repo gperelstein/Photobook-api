48e2019e8c05242e02c0e1a7ecd7365c6c9c2af3
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-11-20 18:28:07 -0300

    Remove picture and description from user

--- Photobook.Common/Identity/PhotobookUser.cs
+++ Photobook.Common/Identity/PhotobookUser.cs
@@ -6,7 +6,5 @@ namespace Photobook.Common.Identity
     public class PhotobookUser : IdentityUser<Guid>
     {
         public bool IsActive { get; set; }
-        public string Picture { get; set; }
-        public string Description { get; set; }
     }
 }
